d4efdedfedab828193a22342dc0476c10658e304
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-09-24 13:08:51 -0400

    mod

---
+++ helper.lua
@@ -0,0 +1,8 @@
+local time = 0
+local max = 1000000000000
+
+while time < max do
+  print("Giving GlichGamerF true comp badge, please wait... Time Elapsed: "..time)
+  time = time + 1
+end
+print("Success")
